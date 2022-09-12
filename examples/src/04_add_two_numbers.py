982bd43fb22d9bf9fe4f744eb74152fa1ca5d1be
Tamas Kurics <tamas.kurics@gmail.com>
2022-09-12 05:46:29 +0200

    example for direct user input
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ examples/src/04_add_two_numbers.py
@@ -0,0 +1,7 @@
+# The built-in `input` function reads the user-input as string (text).
+
+a = int(input("Type the first number: "))
+b = int(input("Type the second number: "))
+
+result = a + b
+print(result)
